698013904b2acf0c736fb0c7fe1b22d781573a86
Jessica Carrillo <jessymamo@gmail.com>
2023-07-17 18:32:10 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// document.body.append(element);
+
+let idMain = document.querySelector('main')
+idMain.remove()
+
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Jessica is the champion"
